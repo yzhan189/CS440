4422c695dc1664e915f958198923de13b7686f96
Jingtao Wu <jentaowoo@gmail.com>
2017-09-24 19:57:49 -0500

    b/dfs

--- src/searches/Pair.java
+++ src/searches/Pair.java
@@ -1,4 +1,25 @@
 package searches;
 
-public class Pair {
+public class Pair <T, K> {
+
+    private final T left;
+    private final K right;
+
+    public Pair(T left, K right){
+        this.left = left;this.right = right;
+    }
+
+    public T getLeft() {
+            return left;
+        }
+
+    public K getRight() {
+            return right;
+        }
+
+    public boolean equals(Pair p){
+            return this.left.equals(p.getLeft()) && this.right.equals(p.getRight());
+        }
+
+
 }
--- src/searches/part1.java
+++ src/searches/part1.java
@@ -1,4 +1,95 @@
 package searches;
 
-public class bfs {
+import maze.Maze;
+import maze.Node;
+
+import java.io.IOException;
+import java.util.*;
+
+public class part1 {
+    public static String bfs(Node root){
+        Queue<Pair<Node, String>> queue = new LinkedList<>();
+
+        Set<Node> visited = new HashSet<>();
+
+        queue.add(new Pair<>(root,""));
+
+        while(!queue.isEmpty()){
+            Pair<Node, String> current = queue.poll();
+            Node node = current.getLeft();
+            String steps = current.getRight();
+            if(visited.contains(node)){
+                continue;
+            }
+            if(node.getType()=='.'){
+                return steps;
+            }
+            if(node.getType() ==' ' || node.getType()=='P') {
+                visited.add(current.getLeft());
+                queue.add(new Pair<>(node.getNorth(), steps+"n"));
+                queue.add(new Pair<>(node.getSouth(), steps+"s"));
+                queue.add(new Pair<>(node.getWest(), steps+"w"));
+                queue.add(new Pair<>(node.getEast(), steps+"e"));
+            }
+        }
+        return "";
+    }
+
+
+    public static String dfs(Node root){
+        Stack<Pair<Node, String>> stack = new Stack<>();
+
+        Set<Node> visited = new HashSet<>();
+        stack.add(new Pair<>(root,""));
+
+        while(!stack.isEmpty()) {
+            Pair<Node, String> current = stack.pop();
+            Node node = current.getLeft();
+            String steps = current.getRight();
+            if(visited.contains(node)){
+                continue;
+            }
+            if (node.getType() == '.') {
+                return steps;
+            }
+
+            if(node.getType() ==' ' || node.getType()=='P') {
+                visited.add(current.getLeft());
+                stack.push(new Pair<>(node.getNorth(), steps+"n"));
+                stack.push(new Pair<>(node.getSouth(), steps+"s"));
+                stack.push(new Pair<>(node.getWest(), steps+"w"));
+                stack.push(new Pair<>(node.getEast(), steps+"e"));
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args){
+        try {
+            Maze m = new Maze("openMaze");
+            String solution = dfs(m.getRoot());
+            Node node = m.getRoot();
+            while(!solution.isEmpty()){
+                if(solution.charAt(0)=='n'){
+                    node = node.getNorth();
+                }
+                if(solution.charAt(0)=='s'){
+                    node = node.getSouth();
+                }
+                if(solution.charAt(0)=='w'){
+                    node = node.getWest();
+                }
+                if(solution.charAt(0)=='e'){
+                    node = node.getEast();
+                }
+                node.type = '.';
+                solution = solution.substring(1);
+            }
+            m.printMaze();
+
+        } catch (IOException e){
+            System.out.print("Oops something wrong with Maze constructor");
+            return;
+        }
+    }
 }
